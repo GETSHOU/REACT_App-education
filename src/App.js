7af2db66e224d948720fd01d2a2bb43564e46d7f
Borisov Sergei <BorisovSF.mailbox@gmail.com>
2023-05-09 23:50:59 +0300

    CurrentDate component changed (without JSX)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-// декларативный стиль - начало
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { CurrentDateComponent } from './components/CurrentDateComponent';
 
+// export const App = () => {
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<CurrentDateComponent />
+// 			</header>
+// 		</div>
+// 	);
+// };
+
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<CurrentDateComponent />
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement(CurrentDateComponent),
+		),
 	);
 };
-// декларативный стиль - конец
