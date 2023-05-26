1d16e40b8e0276008bac26fa019edbbf8410198f
Borisov Sergei <BorisovSF.mailbox@gmail.com>
2023-05-26 14:20:55 +0300

    my test branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import './clear.css';
 import './App.css';
+import { UserProfile } from './components/userProfile/userProfile.jsx';
+
+const userInfo = {
+	nickName: 'Getshou',
+	name: 'Сергей',
+	lastname: 'Борисов',
+	age: '31',
+	email: 'BorisovSF.mailbox@gmail.com',
+	address: {
+		country: 'РФ',
+		city: 'Москва',
+	},
+	avatar: '',
+};
 
 export const App = () => {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<UserProfile {...userInfo} />
+		</div>
+	);
 };
